f51d55dba0e4970c23ff17d610b10e1caaa503d5
chri_mc_tech <chrimctech@gmail.com>
2026-07-12 00:42:51 +0200

    added basic rendering and improved process list

--- main.cpp
+++ main.cpp
@@ -15,47 +15,92 @@
 using std::cout;
 using std::endl;
 
+namespace ui {
+  int window_width = 1920;
+  int window_height = 1080;
+  Font font;
+  int scroll;
+}
+
+void update_input();
+
+//  cout << std::chrono::high_resolution_clock::now().time_since_epoch().count() << endl;
 
 int main() {
+  // std::jthread thread_test = std::thread(&function_test);
+  SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+  InitWindow(ui::window_width, ui::window_height, "test");
 
-  HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-  PROCESSENTRY32 proc_entry;
+  ui::font = LoadFont("Archivo-SemiBold.ttf");
 
+  process::update_list();
 
-  if (snapshot == INVALID_HANDLE_VALUE) {
-    return 2;
-  }
+  auto last_clock_processes_cpu = std::chrono::steady_clock::now();
+  auto last_clock_processes_update = std::chrono::steady_clock::now();
 
-  proc_entry.dwSize = sizeof(PROCESSENTRY32);
+  SetTargetFPS(60);
 
-  if (!Process32First(snapshot, &proc_entry)) {
-    CloseHandle(snapshot);
-    return 3;
-  }
+  while (!WindowShouldClose()) {
+    auto now = std::chrono::steady_clock::now();
 
-
-  cout << "proc id: " << proc_entry.th32ProcessID << endl;
-  cout << "proc name: " << proc_entry.szExeFile << endl;
-  cout << "parent id: " << proc_entry.th32ParentProcessID  << endl;
-
-  while (Process32Next(snapshot, &proc_entry)) {
-    
-    if (static_cast<std::string>(proc_entry.szExeFile) != "opera.exe") {
-      continue;
+    if (now - last_clock_processes_update >= std::chrono::milliseconds(2000)) {
+      process::update_list();
+      // process::group_children_processes();
+      last_clock_processes_update = now;
     }
-    cout << "proc id: " << proc_entry.th32ProcessID << endl;
-    cout << "proc name: " << proc_entry.szExeFile << endl;
-    cout << "parent id: " << proc_entry.th32ParentProcessID  << endl;
+    if (now - last_clock_processes_cpu >= std::chrono::milliseconds(500)) {
+      std::jthread t(process::update_processes_cpu_usage);
+      t.detach();
+      last_clock_processes_cpu = now;
+    }
 
-    HANDLE process = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, proc_entry.th32ProcessID);
-    cout << process::get_process_percentage(process, 2000) << endl;
+    update_input();
 
+    ClearBackground({20, 20, 20, 255});
+    SetWindowOpacity(1);
+    BeginDrawing();
+    DrawFPS(1700, 10);
 
-    cout << "\n\n";
+    float i = 50;
+    float n = 0;
 
+    DrawLine(0, i - 6, ui::window_width, i - 6, GRAY);
+    DrawLine(0, i - 7, ui::window_width, i - 7, GRAY);
 
+    for (const auto& proc: process::processes) {
+      if (n < ui::scroll) {
+        n++;
+        continue;
+      }
+      if (i > 1000) {break;}
+      DrawTextEx(ui::font, proc.second.name.c_str(), {50, i}, 18, 0.5, WHITE);
+      DrawTextEx(ui::font, std::to_string(proc.second.cpu_usage.load()).c_str(), {450, i}, 18, 0.5, WHITE);
+      DrawTextEx(ui::font, std::to_string(proc.first).c_str(), {600, i}, 18, 0.5, WHITE);
+      DrawTextEx(ui::font, std::to_string(proc.second.parent_id).c_str(), {750, i}, 18, 0.5, WHITE);
+      if (proc.second.sub_proc_number != 0) {
+        DrawTextEx(ui::font, std::to_string(proc.second.sub_proc_number).c_str(), {900, i}, 18, 0.5, WHITE);
+      }
+
+      DrawLine(0, i + 22, ui::window_width, i + 22, GRAY);
+      DrawLine(0, i + 21, ui::window_width, i + 21, GRAY);
+      i += 28;
+
+    }
+    EndDrawing();
   }
-  CloseHandle(snapshot);
+
+}
+
+
+void update_input() {
+
+  ui::scroll -= GetMouseWheelMove();
+  if (ui::scroll < 0) {ui::scroll = 0;}
+
+  if (IsWindowResized()) {
+    ui::window_width = GetScreenWidth();
+    ui::window_height = GetScreenHeight();
+  }
 
 
 }
--- process/process.h
+++ process/process.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <atomic>
+#include <unordered_map>
 
 
 namespace process {
@@ -6,8 +8,27 @@ namespace process {
     unsigned long long kernel;
     unsigned long long user;
   };
+  struct Process {
+    DWORD parent_id;
+    std::string name;
+    std::string path;
+
+    std::atomic<double> cpu_usage;
+
+    HANDLE handle;
+
+    int sub_proc_number;
+
+    proc_times last_time;
+  };
+
+  inline std::unordered_map<DWORD, Process> processes;
 
 
   proc_times get_process_time(HANDLE process);
   double get_process_percentage(HANDLE process, int sleep);
+  void update_list();
+  void update_processes_cpu_usage();
+  std::string get_process_path(HANDLE handle);
+  // void group_children_processes();
 }
